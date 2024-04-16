2a64bb477265b9b3d4f91451403e7cf901dd0694
Denada Rama <denadaramatech@gmail.com>
2024-04-16 17:52:20 +0100

    implemented EDA

--- Contributor_demos/Sarcasm Detection/helpers.py
+++ Contributor_demos/Sarcasm Detection/helpers.py
@@ -1,9 +1,37 @@
 from kaggle.api.kaggle_api_extended import KaggleApi
 import pandas as pd
 import zipfile
+import numpy as np
+import pickle
 
 DATASET = 'danofer/sarcasm'
 DATA_DIR = './data'
+
+
+def save_to_pickle(df, filename):
+    """
+    Save a DataFrame to a pickle file.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame to be saved.
+        filename (str): The name of the file to save the DataFrame to.
+    """
+    with open(filename, 'wb') as f:
+        pickle.dump(df, f)
+
+def load_from_pickle(filename):
+    """
+    Load a DataFrame from a pickle file.
+
+    Args:
+        filename (str): The name of the file to load the DataFrame from.
+
+    Returns:
+        pandas.DataFrame: The loaded DataFrame.
+    """
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
+
 
 def get_kaggle_dataset():
     """
@@ -37,3 +65,29 @@ def get_kaggle_dataset():
                 datasets.append(pd.read_csv(f))
     
     return datasets[0], datasets[1]
+
+
+def show_values_on_bars(axs, h_v="v", space=0.4):
+    '''Plots the value at the end of the a seaborn barplot.
+    axs: the ax of the plot
+    h_v: weather or not the barplot is vertical/ horizontal'''
+    
+    def _show_on_single_plot(ax):
+        if h_v == "v":
+            for p in ax.patches:
+                _x = p.get_x() + p.get_width() / 2
+                _y = p.get_y() + p.get_height()
+                value = int(p.get_height())
+                ax.text(_x, _y, format(value, ','), ha="center") 
+        elif h_v == "h":
+            for p in ax.patches:
+                _x = p.get_x() + p.get_width() + float(space)
+                _y = p.get_y() + p.get_height()
+                value = int(p.get_width())
+                ax.text(_x, _y, format(value, ','), ha="left")
+
+    if isinstance(axs, np.ndarray):
+        for idx, ax in np.ndenumerate(axs):
+            _show_on_single_plot(ax)
+    else:
+        _show_on_single_plot(axs)
